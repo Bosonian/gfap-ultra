3d808b85ef1211d551407cef392f4727ad3ea750
Deepak Bos <bosdeepak@gmail.com>
2025-12-02 05:50:49 +0100

    Updates

--- assets/mountIslands-JQ-D1x2i.js
+++ assets/mountIslands-JQ-D1x2i.js
@@ -1,4 +1,4 @@
-import{g as Lc}from"./index-D4x884w9.js";var hs={exports:{}},T={};/**
+import{g as Lc}from"./index-vMAGNVMP.js";var hs={exports:{}},T={};/**
  * @license React
  * react.production.min.js
  *
